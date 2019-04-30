e5b49a5f38effe67c40de95b4ee3f696a862a081
AdamLionB <lion.adam.otman@outlook.fr>
2019-04-30 11:55:01 +0200

    cleaned file, now can be printed

--- conllu_reader.py
+++ conllu_reader.py
@@ -2,23 +2,36 @@ from nltk.corpus.reader.conll import ConllCorpusReader
 
 
 keys = ['word', 'lemma', 'POS', 'A', 'flexion', 'refs', 'B', 'C', 'D']
+prefix_len = len('# text = ')
+
 
 class Conllu_file:
-    def __init__(self, file_path):
-        conllu_sentences = []
+    def __init__(self, file_path, limit=0):
+        self.sentences = []
         with open(file_path, encoding='utf-8') as file:
             for line in file:
-                conllu_sentences.append(Conllu_sentence(file))
-                if len(conllu_sentences) ==3 : break
+                self.sentences.append(Conllu_sentence(line, file))
+                if len(self.sentences) == limit : break
+    def __repr__(self):
+        st = ''
+        for sentence in self.sentences:
+            st += str(sentence)
+        return st
 
 class Conllu_sentence:
-    def __init__(self, file):
-        tokens = {}
+    def __init__(self,sentence, file):
+        self.rows = {}
+        self.sentence = sentence[prefix_len:]
         for line in file:
             if line == '\n' or line == '\n\r' : break
-            columns = line[:-1].split('\t')
-            tokens[columns[0]] = {k:v for k,v in zip(keys,columns[1:])}
-        print(tokens)
-
-
-Conllu_file('fr-common_crawl-164.conllu')
+            cells = line[:-1].split('\t')
+            self.rows[cells[0]] = {k:v for k,v in zip(keys,cells[1:])}
+    def __repr__(self):
+        print(len(self.rows))
+        st = self.sentence + '\n'
+        for k,row in self.rows.items():
+            st += k+'\t|\t' 
+            for cell in row.values():
+                st += cell + '\t'
+            st += '\n'
+        return st+'\n'
