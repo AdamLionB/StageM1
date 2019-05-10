a42a081509f199fee17574416a7a7b91806b5789
AdamLionB <lion.adam.otman@outlook.fr>
2019-05-10 16:32:01 +0200

    bad sentences trasher

--- conllu_reader.py
+++ conllu_reader.py
@@ -1,32 +1,44 @@
 import pandas as pd
 import numpy as np
+import re
 
 keys = ['Id', 'Form', 'Lemma', 'UPosTag', 'XPosTagA', 'Feats', 'Head', 'DepRel', 'Deps', 'Misc']
 prefix_len = len('# text = ')
+
 
 def read_conllu(path, start = 0, end = -1):
     if end > 0 and start - end >= 0 : return
     file = open(path, encoding='utf-8')
     sentences = []
-    a = []
+    all_rows = []
     i = 0
+    keep = True
     for line in file:
+        s_rows = []
         if i < start:
             for l in file:
                 if l == '\n' or l == '\n\r':
                     break
         else:
             if i >= end : break
-            b = []
-            s = line[prefix_len:]
-            sentences.append(s)
+            row = []
+            sentences.append(line[prefix_len:])
             for l in file:
                 if l == '\n' or l == '\n\r' :break
                 cells = l[:-1].split('\t')
-                b = [i] + [v for v in cells]
-                a.append(b)
+                row = [i] + [v for v in cells]
+                if row[4] == 'NOUN' or row[4] == 'VERB':
+                    if re.match(r'\W', row[3], flags = re.UNICODE) :
+                        keep = False
+                s_rows.append(row)
+        if keep: all_rows += s_rows
+        else :
+            sentences = sentences[:-1]
+            keep = True
         i+=1
-    c = pd.DataFrame(data = a)
-    c.columns = ['SId'] + keys
-    c.set_index(['SId', 'Id'], inplace = True)
-    return c, sentences
+    frame = pd.DataFrame(data = all_rows)
+    frame.columns = ['SId'] + keys
+    frame.set_index(['SId', 'Id'], inplace = True)
+    return frame, sentences
+
+
